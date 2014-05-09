fd66c54a2d0b74a79795be137abacd0256936368
David Silva <davidslv@gmail.com>
2014-05-09 17:54:08 +0100

    Fixes identation

--- spec/bitmap_editor/image_spec.rb
+++ spec/bitmap_editor/image_spec.rb
@@ -27,96 +27,95 @@ module BitmapEditor
           image.pixels.flatten.should have(62500).pixels
         end
       end
+    end
 
-      describe "#show" do
-        subject { described_class.new(5,5) }
+    describe "#show" do
+      subject { described_class.new(5,5) }
 
-        its(:show) { should eql("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO") }
+      its(:show) { should eql("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO") }
+    end
+
+    describe "#colour" do
+      it 'colours the image with A at X1 Y2' do
+        #   1 2 3 4
+        # 1 A O O O
+        # 2 O O O O
+        # 3 O O O O
+        # 4 O O O O
+        image.colour(1, 2, 'A')
+        image.show.should eql("OOOO\nAOOO\nOOOO\nOOOO")
       end
+    end
 
-      describe "#colour" do
-        it 'colours the image with A at X1 Y2' do
-          #   1 2 3 4
-          # 1 A O O O
-          # 2 O O O O
-          # 3 O O O O
-          # 4 O O O O
-          image.colour(1, 2, 'A')
-          image.show.should eql("OOOO\nAOOO\nOOOO\nOOOO")
-        end
+    describe "#clear" do
+      it 'clears the image to default value O' do
+        image.colour(2, 2, 'B')
+        image.clear
+        image.show.should eql("OOOO\nOOOO\nOOOO\nOOOO")
       end
+    end
 
-      describe "#clear" do
-        it 'clears the image to default value O' do
-          image.colour(2, 2, 'B')
-          image.clear
-          image.show.should eql("OOOO\nOOOO\nOOOO\nOOOO")
-        end
+    describe "#vertical" do
+      it 'draws vertically between Y1 and Y2 in column X' do
+        #   1 2 3 4
+        # 1 O O O O
+        # 2 X O O O
+        # 3 X O O O
+        # 4 X O O O
+        image.vertical(1, 2, 4, 'X')
+        image.show.should eql("OOOO\nXOOO\nXOOO\nXOOO")
       end
+    end
 
-      describe "#vertical" do
-        it 'draws vertically between Y1 and Y2 in column X' do
-          #   1 2 3 4
-          # 1 O O O O
-          # 2 X O O O
-          # 3 X O O O
-          # 4 X O O O
-          image.vertical(1, 2, 4, 'X')
-          image.show.should eql("OOOO\nXOOO\nXOOO\nXOOO")
-        end
+    describe "#horizontal" do
+      it 'draws horizontally between X1 and X2 in row Y' do
+        #   1 2 3 4
+        # 1 O O O O
+        # 2 O O O O
+        # 3 R R R O
+        # 4 O O O O
+        image.horizontal(1, 3, 3, 'R')
+        image.show.should eql("OOOO\nOOOO\nRRRO\nOOOO")
       end
+    end
 
-      describe "#horizontal" do
-        it 'draws horizontally between X1 and X2 in row Y' do
-          #   1 2 3 4
-          # 1 O O O O
-          # 2 O O O O
-          # 3 R R R O
-          # 4 O O O O
-          image.horizontal(1, 3, 3, 'R')
-          image.show.should eql("OOOO\nOOOO\nRRRO\nOOOO")
-        end
-      end
+    describe "#fill" do
+      # Fills the region R with the colour C. R is defined as: Pixel (X,Y) belongs to R.
+      # Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
 
-      describe "#fill" do
-        # Fills the region R with the colour C. R is defined as: Pixel (X,Y) belongs to R.
-        # Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
+      context 'when there is a "blank" image' do
 
-        context 'when there is a "blank" image' do
-
-          it 'fills the entire image with color E' do
-            # INITIAL     # FILL
-            #   1 2 3 4   #   1 2 3 4
-            # 1 O O O O   # 1 E E E E
-            # 2 O O O O   # 2 E E E E
-            # 3 O O O O   # 3 E E E E
-            # 4 O O O O   # 4 E E E E
-
-            image.fill(2, 3, 'E')
-            image.show.should eql("EEEE\nEEEE\nEEEE\nEEEE")
-          end
-        end
-
-        context 'when there is already one pixel filled' do
+        it 'fills the entire image with color E' do
           # INITIAL     # FILL
           #   1 2 3 4   #   1 2 3 4
           # 1 O O O O   # 1 E E E E
-          # 2 O A O O   # 2 E A E E
+          # 2 O O O O   # 2 E E E E
           # 3 O O O O   # 3 E E E E
           # 4 O O O O   # 4 E E E E
 
-          before do
-            image.colour(2, 2, 'A')
-          end
+          image.fill(2, 3, 'E')
+          image.show.should eql("EEEE\nEEEE\nEEEE\nEEEE")
+        end
+      end
 
-          it 'fills all other pixels color E' do
-            image.fill(4, 4, 'E')
-            image.show.should eql("EEEE\nEAEE\nEEEE\nEEEE")
-          end
+      context 'when there is already one pixel filled' do
+        # INITIAL     # FILL
+        #   1 2 3 4   #   1 2 3 4
+        # 1 O O O O   # 1 E E E E
+        # 2 O A O O   # 2 E A E E
+        # 3 O O O O   # 3 E E E E
+        # 4 O O O O   # 4 E E E E
+
+        before do
+          image.colour(2, 2, 'A')
         end
 
+        it 'fills all other pixels color E' do
+          image.fill(4, 4, 'E')
+          image.show.should eql("EEEE\nEAEE\nEEEE\nEEEE")
+        end
       end
+
     end
   end
-
 end
