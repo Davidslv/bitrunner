f91d3826578949c0b9215497960f1882d96bc240
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-11-07 08:39:21 +0000

    Separate Fill Region into own file
    Also changes Image to Bitmap

---
+++ lib/bitmap_editor/commands/fill_region.rb
@@ -0,0 +1,29 @@
+module BitmapEditor
+  class FillRegion
+    attr_reader :bitmap, :x, :y, :c
+
+    def initialize(bitmap, x, y, c)
+      @bitmap = bitmap
+      @x, @y, @c = x, y, c
+    end
+
+    def perform
+      _fill(x, y, c)
+    end
+
+    private
+
+    def _fill(x, y, c)
+      return if (x < MIN_DIMENSION || x == @width)
+      return if (y < MIN_DIMENSION || y == @height)
+      return if (pixels[y][x] == c) || (pixels[y][x] != 'O')
+
+      pixels[y][x] = c
+
+      flood_fill(x - 1, y, c)
+      flood_fill(x + 1, y, c)
+      flood_fill(x, y + 1, c)
+      flood_fill(x, y - 1, c)
+    end
+  end
+end
