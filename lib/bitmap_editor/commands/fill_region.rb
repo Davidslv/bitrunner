92f1910976161a0ed072c290ab7eb9cb7b23f9a1
David Silva <davidslv@gmail.com>
2017-11-08 21:02:18 +0000

    Properly namespace FillRegion command

--- lib/bitmap_editor/commands/fill_region.rb
+++ lib/bitmap_editor/commands/fill_region.rb
@@ -1,29 +1,31 @@
 module BitmapEditor
-  class FillRegion
-    attr_reader :bitmap, :x, :y, :c
+  module Commands
+    class FillRegion
+      attr_reader :bitmap, :x, :y, :c
 
-    def initialize(bitmap, x, y, c)
-      @bitmap = bitmap
-      @x, @y, @c = x, y, c
-    end
+      def initialize(bitmap, x, y, c)
+        @bitmap = bitmap
+        @x, @y, @c = x, y, c
+      end
 
-    def perform
-      _fill(x, y, c)
-    end
+      def perform
+        _fill(x, y, c)
+      end
 
-    private
+      private
 
-    def _fill(x, y, c)
-      return if (x < MIN_DIMENSION || x == @width)
-      return if (y < MIN_DIMENSION || y == @height)
-      return if (pixels[y][x] == c) || (pixels[y][x] != 'O')
+      def _fill(x, y, c)
+        return if (x < MIN_DIMENSION || x == @width)
+        return if (y < MIN_DIMENSION || y == @height)
+        return if (pixels[y][x] == c) || (pixels[y][x] != 'O')
 
-      pixels[y][x] = c
+        pixels[y][x] = c
 
-      flood_fill(x - 1, y, c)
-      flood_fill(x + 1, y, c)
-      flood_fill(x, y + 1, c)
-      flood_fill(x, y - 1, c)
+        flood_fill(x - 1, y, c)
+        flood_fill(x + 1, y, c)
+        flood_fill(x, y + 1, c)
+        flood_fill(x, y - 1, c)
+      end
     end
   end
 end
