c654af29b08bd131d37212d0283671d5e47e5122
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-11-11 13:10:07 +0000

    Add #build into BitmapHelpers
    This method allows to build a bitmap based on the structure given,
    instead of calling Bitmap#set multiple times to set colours in different coordinates.

--- spec/support/bitmap_helpers.rb
+++ spec/support/bitmap_helpers.rb
@@ -1,4 +1,12 @@
 module BitmapHelpers
+  def build(bitmap, structure)
+    structure.each_with_index do |row, y|
+      row.each_with_index do |color, x|
+        bitmap.set(x, y, color)
+      end
+    end
+  end
+
   def assert_equal_bitmap(bitmap, expected)
     expected.each_with_index do |row, y|
       row.each_with_index do |_column, x|
