127ed938f91dbf1c1b2d4255d998eb0f0db73707
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-11-11 15:17:29 +0000

    Improve spec for DrawVertical

--- spec/bitmap_editor/commands/draw_vertical_spec.rb
+++ spec/bitmap_editor/commands/draw_vertical_spec.rb
@@ -5,17 +5,12 @@ require './lib/bitmap_editor/commands/draw_vertical'
 module BitmapEditor
   module Commands
     RSpec.describe DrawVertical do
-      context 'when receives coordinates' do
+      context 'when it is a bitmap with 3 x 4 dimensions' do
         let(:bitmap) { Bitmap.new(3, 4) }
-        let(:subject) { described_class.new(bitmap, 1, 0, 2, 'A') }
 
-        #   0 1 2
-        # 0 O A O
-        # 1 O A O
-        # 2 O A O
-        # 3 O O O
+        it 'colors the bitmap on 2nd column between Y1 and Y2' do
+          subject = DrawVertical.new(bitmap, 1, 0, 2, "A")
 
-        it 'should color the coordinates between given x coordinates' do
           expected = [
             %w(O A O),
             %w(O A O),
@@ -28,30 +23,46 @@ module BitmapEditor
         end
       end
 
-      context 'when receives coordinates' do
+      context 'when it is a bitmap with 6 x 4 dimensions' do
         let(:bitmap) { Bitmap.new(6, 4) }
         let(:subject) { described_class }
 
-        #   0 1 2 3 4 5
-        # 0 A O O C O O
-        # 1 A O O C O D
-        # 2 A O B O O D
-        # 3 O O B O O O
+        context 'and there are more than one vertical segment' do
+          it 'colors the given coordinates' do
+            expected = [
+              %w(A O O C O O),
+              %w(A O O C O D),
+              %w(A O B O O D),
+              %w(O O B O O O),
+            ]
 
-        it 'should color the coordinates between given x xx coordinates' do
-          expected = [
-            %w(A O O C O O),
-            %w(A O O C O D),
-            %w(A O B O O D),
-            %w(O O B O O O),
-          ]
+            subject.new(bitmap, 0, 0, 2, 'A').perform
+            subject.new(bitmap, 2, 2, 3, 'B').perform
+            subject.new(bitmap, 3, 0, 1, 'C').perform
+            subject.new(bitmap, 5, 1, 2, 'D').perform
 
-          subject.new(bitmap, 0, 0, 2, 'A').perform
-          subject.new(bitmap, 2, 2, 3, 'B').perform
-          subject.new(bitmap, 3, 0, 1, 'C').perform
-          subject.new(bitmap, 5, 1, 2, 'D').perform
+            assert_equal_bitmap(bitmap, expected)
+          end
 
-          assert_equal_bitmap(bitmap, expected)
+          it 'overrides any previous colors given by the new vertical coordinates' do
+            expected = [
+              %w(A O O B O O),
+              %w(A O O B O A),
+              %w(A O B O O A),
+              %w(O O B O O O),
+            ]
+
+            subject.new(bitmap, 0, 0, 2, 'A').perform
+            subject.new(bitmap, 2, 2, 3, 'B').perform
+            subject.new(bitmap, 3, 0, 1, 'C').perform
+            subject.new(bitmap, 5, 1, 2, 'D').perform
+
+            # override colors C & D
+            subject.new(bitmap, 3, 0, 1, 'B').perform
+            subject.new(bitmap, 5, 1, 2, 'A').perform
+
+            assert_equal_bitmap(bitmap, expected)
+          end
         end
       end
     end
