47328ae19be5ef9b6aac23b344c03e7ef8ecebe2
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-11-13 17:49:00 +0000

    Improve recursive algorithm in FillRegion
    This commit marks the visited coordinates before it validates,
    so it doesn't have to validate the same coordinate again if the same
    coodinate is called again during recursion.
    
    It also adds functionality to check diagonal coordinates.

--- lib/bitmap_editor/commands/fill_region.rb
+++ lib/bitmap_editor/commands/fill_region.rb
@@ -15,16 +15,28 @@ module BitmapEditor
       private
       attr_reader :bitmap, :x, :y, :c, :original_color
 
-      def _fill(x, y, c)
+      def _fill(x, y, c, visited = {})
+        # Marks the visited coordinates before it validates
+        # so it doesn't have to validate the same coordinate again.
+        visited[x] ||= {}
+        return if visited[x][y]
+        visited[x][y] = true
+
         return unless bitmap.valid_coordinates?(x, y)
 
         if bitmap.get(x, y) == original_color
           bitmap.set(x, y, c)
 
-          _fill(x - 1, y, c) # west
-          _fill(x + 1, y, c) # east
-          _fill(x, y + 1, c) # south
-          _fill(x, y - 1, c) # north
+          _fill(x, y - 1, c, visited)      # north
+          _fill(x, y + 1, c, visited)      # south
+
+          _fill(x - 1, y, c, visited)      # west
+          _fill(x - 1, y - 1, c, visited)  # north west
+          _fill(x - 1, y + 1, c, visited)  # south west
+
+          _fill(x + 1, y, c, visited)      # east
+          _fill(x + 1, y - 1, c, visited)  # north east
+          _fill(x + 1, y + 1, c, visited)  # south east
         end
       end
     end
