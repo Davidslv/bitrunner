252a8fb81dca6de72f87f2d758cdf9d82a822143
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-11-14 11:33:13 +0000

    Validate coordinates on Bitmap#get and Bitmap#set
    This commit adds OutOfBoundariesError so it can be raised when the input coordinates are
    not within the Bitmap boundaries.

--- spec/bitmap_editor/bitmap_spec.rb
+++ spec/bitmap_editor/bitmap_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require './lib/bitmap_editor/validators/base'
+require './lib/bitmap_editor/validators/coordinates_validator'
+require './lib/bitmap_editor/errors/out_of_boundaries_error'
 require './lib/bitmap_editor/bitmap'
 
 module BitmapEditor
@@ -6,16 +9,48 @@ module BitmapEditor
     let(:bitmap) { Bitmap.new(3, 4) }
 
     describe '#get' do
-      it 'should return the colour of a given coordinate' do
-        expect(bitmap.get(2, 2)).to eql 'O'
+      context 'when within bitmap boundaries' do
+        it 'returns the colour of a given coordinate' do
+          expect(bitmap.get(2, 2)).to eql 'O'
+        end
+      end
+
+      context 'when outside bitmap boundaries' do
+        it 'raises an OutOfBoundariesError error' do
+          expected_message = %Q{
+            Coordinates X: -1 and Y: -1 are out of the bitmap boundaries,
+            coordinate X should be between 0 and 3
+            coordinate Y should be between 0 and 4
+          }
+
+          expect {
+            bitmap.set(-1, -1, 'A')
+          }.to raise_error(Errors::OutOfBoundariesError, expected_message)
+        end
       end
     end
 
     describe '#set' do
-      it 'should set a coordinate with a given colour' do
-        bitmap.set(1, 1, 'A')
+      context 'when within bitmap boundaries' do
+        it 'sets a coordinate with a given colour' do
+          bitmap.set(1, 1, 'A')
 
-        expect(bitmap.get(1, 1)).to eql 'A'
+          expect(bitmap.get(1, 1)).to eql 'A'
+        end
+      end
+
+      context 'when outside bitmap boundaries' do
+        it 'raises an OutOfBoundariesError error' do
+          expected_message = %Q{
+            Coordinates X: -1 and Y: -1 are out of the bitmap boundaries,
+            coordinate X should be between 0 and 3
+            coordinate Y should be between 0 and 4
+          }
+
+          expect {
+            bitmap.set(-1, -1, 'A')
+          }.to raise_error(Errors::OutOfBoundariesError, expected_message)
+        end
       end
     end
 
